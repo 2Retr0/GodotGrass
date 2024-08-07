22c2d617775c2c43fd85afa300b829233fbf700a
Ethan Truong <18603664+2Retr0@users.noreply.github.com>
2024-08-07 16:05:46 -0700

    Improve player materials and animations

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -9,6 +9,7 @@ extends CharacterBody3D
 var gravity: float = ProjectSettings.get_setting('physics/3d/default_gravity')
 var blend_factor := 0.0
 
+@onready var model : Node3D = $Reisen
 @onready var camera_offset_z : float = $CameraMount/Camera3D.position.z
 @onready var camera_rotation := Vector2($CameraMount.rotation_degrees.x, $CameraMount.rotation_degrees.y)
 
@@ -27,11 +28,11 @@ func _input(event: InputEvent) -> void:
 func _process(delta: float) -> void:	
 	if velocity.length_squared() != 0.0:
 		var angle_offset_velocity : float = velocity.signed_angle_to(-$CameraMount.global_basis.z * Vector3(1,0,1), Vector3.UP)
-		$Reisen.rotation.y = lerp_angle($Reisen.rotation.y, $CameraMount.rotation.y + PI - angle_offset_velocity, minf(delta*5.0, 1.0))
+		model.rotation.y = lerp_angle(model.rotation.y, $CameraMount.rotation.y + PI - angle_offset_velocity, minf(delta*5.0, 1.0))
 	
 	# Add procedural leaning based on velocity
-	var angle_offset_z : float = clamp(-$Reisen.global_basis.z.cross(velocity).y * 0.02, -0.1, 0.1) * PI
-	$Reisen.rotation.z = lerp_angle($Reisen.rotation.z, angle_offset_z, minf(delta*3.0, 1.0))
+	var angle_offset_z : float = clamp(-model.global_basis.z.cross(velocity).y * 0.02, -0.1, 0.1) * PI
+	model.rotation.z = lerp_angle(model.rotation.z, angle_offset_z, minf(delta*3.0, 1.0))
 	
 	# Smooth camera distance transition
 	$CameraMount/Camera3D.position.z = lerpf($CameraMount/Camera3D.position.z, camera_offset_z, minf(delta*5.0, 1.0))
